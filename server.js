4fc34b01e0c6c479bca0d2179a64245eff8cec12
extrude575757 <nicholas.a.lacapria@gmail.com>
2021-10-31 03:38:21 -0700

    Post console.log example

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 5045;
-
+var todoNextId = 1;
+ 
 var todos = [{
     id:1,
     description:'Meet dead pigs at lunch',
@@ -16,6 +18,7 @@ var todos = [{
     completed: true
 }];
 
+app.use(bodyParser.json());
 app.get('/', function(req,res){
     res.send('Todo API Root');
 });
@@ -40,6 +43,14 @@ app.get('/todos/:id', function (req,res){
         res.status(404).send();
     }
     // res.send('Asking for todo with id of ' + req.params.id);
+});
+
+// POST / todos
+app.post('/todos',function(req,res){
+    var body = req.body;
+
+    console.log('Description: ' + body.description);
+    res.json(body);
 });
 
 
