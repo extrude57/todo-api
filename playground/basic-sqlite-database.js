a773dd5b4ca1f5a7c1c2eaf2b8588750cc52681c
extrude575757 <nicholas.a.lacapria@gmail.com>
2021-10-31 16:52:12 -0700

    Added to it now

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -1,9 +1,13 @@
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize(undefined, undefined, undefined,{
+// var sequelize = new Sequelize('sqlite::memory:'){
+//     'dialect' : 'sqlite',
+//     'storage' : __dirname + '/basic-sqlite-database.sqlite'
+// };
+// const sequelize = new Sequelize('sqlite::memory:');
+var sequelize = new Sequelize( {
     'dialect' : 'sqlite',
     'storage' : __dirname + '/basic-sqlite-database.sqlite'
 });
-
 var Todo = sequelize.define('todo', {
     description:{
         type:Sequelize.STRING,
@@ -20,36 +24,45 @@ var Todo = sequelize.define('todo', {
 });
 
 sequelize.sync({
-    force:true
+    // force:true
         }).then(function () {
     console.log('Everything is sycned');
-        Todo.create({
-            description:'Take out trash',
-            completed:false
-        }).then(function(todo){
-            return Todo.create({
-                description:'Clean office'
-            });
-            // console.log('Finished Here');
-            // console.log(todo);
-        }).then(function(){
-            return Todo.findAll({
-                where:{
-                    description:{
-                        $like: '%trash%'
-                    }
+            Todo.findByPk(1).then(function (todo){
+                if (todo){
+                    console.log(todo.toJSON());
+                }else {
+                    console.log('Todo Not found');
                 }
             })
-        }).then(function(todos){
-            if (todos){
-                todos.forEach(function(todo){
-                    console.log(todo.toJSON());
-                });
-                // console.log(todo.toJSON());
-            }else{
-                console.log('No todo found!');
-            }
-        }).catch(function(e){
-            console.log(e);
+
+
         });
-});
+        // Todo.create({
+        //     description:'Take out trash',
+        //     completed:false
+        // }).then(function(todo){
+        //     return Todo.create({
+        //         description:'Clean office'
+        //     });
+        //     // console.log('Finished Here');
+        //     // console.log(todo);
+        // }).then(function(){
+        //     return Todo.findAll({
+        //         where:{
+        //             description:{
+        //                 $like: '%trash%'
+        //             }
+        //         }
+        //     })
+        // }).then(function(todos){
+        //     if (todos){
+        //         todos.forEach(function(todo){
+        //             console.log(todo.toJSON());
+        //         });
+        //         // console.log(todo.toJSON());
+        //     }else{
+        //         console.log('No todo found!');
+        //     }
+        // }).catch(function(e){
+        //     console.log(e);
+        // });
